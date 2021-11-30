2b97eaf0a6f3f8204b5ce1306ddaad19afbfccd7
Sander <sander.paasalu@khk.ee>
2021-11-30 13:37:18 +0200

    testing user data for database connection

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 $db_server = 'localhost';
 $db_andmebaas = 'tiim1ita19iktkhk_Spordiosakond';
-$db_kasutaja = 'mfer';
+$db_kasutaja = 'tiim1ita19iktkhk_mfer';
 $db_salasona = 'qwertyqwertyqwerty';
 //ühendus andmebaasiga
-$yhendus = new mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 //ühenduse kontroll
 if(!$yhendus){
     die('Ei saa ühendust andmebaasiga');
@@ -12,3 +12,4 @@ if(!$yhendus){
 else{
     echo("<h1>db connection</h1>");
 }
+?>
