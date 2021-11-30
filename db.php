e2254fe2520ea1340c4d368143088bf892585e02
Sander <sander.paasalu@khk.ee>
2021-11-30 13:51:12 +0200

    testing user data for database connection

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $db_salasona = 'qwertyqwertyqwerty';
 $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 //ühenduse kontroll
 if(!$yhendus){
-    die('Ei saa ühendust andmebaasiga');
+    die('Ei saa ühendust andmebaasiga' + mysqli_connect_error());
 }
 else{
     echo("<h1>db connection</h1>");
