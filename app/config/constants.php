138966eedf24d4491fb13b12019a0b1309439a1b
Sander <sander.paasalu@khk.ee>
2021-12-02 10:44:58 +0200

    WIP 2.0

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 // Project name
-define('PROJECT_NAME', 'kpr');
+define('PROJECT_NAME', 'spordiosakond');
 // App root dir
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL root
@@ -8,6 +8,6 @@ define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 
 // db params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'kpr');
+define('DB_USER', 'tiim1ita19iktkhk_mfer');
+define('DB_PASS', 'qwertyqwertyqwerty');
+define('DB_NAME', 'tiim1ita19iktkhk_Spordiosakond');
