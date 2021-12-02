4c9d612b200f16f47e0de6493624f7bc542f5d26
Sander <sander.paasalu@khk.ee>
2021-12-02 09:59:31 +0200

    Made some basic login related stuff

--- db.php
+++ db.php
@@ -1,16 +1,18 @@
 <?php
-$db_server = 'localhost';
-$db_andmebaas = 'tiim1ita19iktkhk_Spordiosakond';
-$db_kasutaja = 'tiim1ita19iktkhk_mfer';
-$db_salasona = 'qwertyqwertyqwerty';
-//ühendus andmebaasiga
-$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
-//ühenduse kontroll
-if(!$yhendus){
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
-else{
-    echo("<h1>db connection</h1>");
+function dbConnection(){
+    $db_server = 'localhost';
+    $db_andmebaas = 'tiim1ita19iktkhk_Spordiosakond';
+    $db_kasutaja = 'tiim1ita19iktkhk_mfer';
+    $db_salasona = 'qwertyqwertyqwerty';
+    //ühendus andmebaasiga
+    $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+    //ühenduse kontroll
+    if(!$yhendus){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }
+    else{
+        echo("<a href='login.php'>You are not supposed to be here</a>");
+    }
 }
 ?>
