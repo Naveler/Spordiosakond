0468d5c7cb10f27e802b3c0c1d0f497672742846
Sander <sander.paasalu@khk.ee>
2021-12-09 10:19:50 +0200

    Login Test with db

--- Scripts/login.php
+++ Scripts/login.php
@@ -3,12 +3,11 @@ require 'db.php';
 dbConnection();
 
 
-function getUser($username, $password){
     $perenimi= $_POST[eesnimi ];
     $eesnimi= $_POST[perenimi];
     $password= $_POST[parool];
     $query = mysqli_query("Select * FROM Kasutaja WHERE eesnimi='.$eesnimi.' AND perenimi='.$perenimi.'");
     echo($query);
-}
-getUser();
-?>
+    if(!$query){
+        var_dump("You little shitbag");
+    }
