f47fb845c25f5cab85a48dd499ec27f1e69efbf8
Naveler <georg.vilja@khk.ee>
2021-12-13 11:26:53 +0200

    ooga booga

--- Pages/LoginPage.php
+++ Pages/LoginPage.php
@@ -6,15 +6,15 @@
     <h1 class="position">GHG</h1>
     <div class="position">
         <div class="form-element">
-            <input type="text" name="email" placeholder="email"  required />
+            <input class="textField" type="text" name="email" placeholder="email"  required />
         </div>
     <div class="position">
         <div class="form-element">
-            <input type="password" name="parool" placeholder="password"  required />
+            <input class="textField" type="password" name="parool" placeholder="password"  required />
         </div>
     </div>
     <input class="button" type="submit" value="Logi sisse">
-    <h4>Pole kontot? loo konto</h4>
+    <h4>Pole kontot? loo konto <a href="#">siin</a></h4>
 </div>
 </form>
 </body>
