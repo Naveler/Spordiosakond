4c01e3021ca90c55d280776286590f65461b9a36
Naveler <georg.vilja@khk.ee>
2021-12-15 13:36:23 +0200

    html change, Sander will be mad

--- Pages/LoginPage.php
+++ Pages/LoginPage.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <body>
 <form method="POST" action="../Scripts/login.php">
+<link rel="stylesheet" href="./CSS/variables.css">
 <link rel="stylesheet" href="CSS/login.css" />
-<div class="container">
-    <h1 class="position">GHG</h1>
-    <div class="position">
-        <div class="form-element">
-            <input class="textField" type="text" name="email" placeholder="email"  required />
-        </div>
-    <div class="position">
-        <div class="form-element">
-            <input class="textField" type="password" name="parool" placeholder="password"  required />
-        </div>
-    </div>
-    <input class="button" type="submit" value="Logi sisse">
-    <h4>Pole kontot? loo konto <a href="#">siin</a></h4>
+<div class="background">
+    <form>
+        <input type="text" placeholder="Email" name="email" required>
+        <input type="password" name="parool" placeholder="Parool" required>
+        <input class="button" type="submit" value="Logi sisse">
+    </form>
 </div>
-</form>
 </body>
