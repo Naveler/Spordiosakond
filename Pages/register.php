a60ffa5e30499c2a9002364afeabe31e1ddcc397
Sander <sander.paasalu@khk.ee>
2022-01-13 10:36:33 +0200

    Working registering

--- Pages/register.php
+++ Pages/register.php
@@ -22,10 +22,17 @@ if ($_SESSION["status"]=='active') {
     </header>
     <div class="container flex">
         <h1 class="smth center">Loo konto</h1>
-        <form action="register.php" method="POST" class="flex">  
-            <input type="text" class="form-input no-outline" placeholder="Email" required>
-            <input type="text" class="form-input no-outline" placeholder="Parool" required>
-            <input type="text" class="form-input no-outline" placeholder="Korda parool" required>
+        <form action="../Scripts/register.php" method="POST" class="flex">
+            <input type="text" class="form-input no-outline" placeholder="email" name="email" required>
+            <input type="text" class="form-input no-outline" placeholder="eesnimi" name="eesnimi" required>
+            <input type="text" class="form-input no-outline" placeholder="perenimi" name="perenimi" required>
+            <select class="form-input no-outline" name="role" required>
+                <option value="Õpilane">Õpilane</option>
+                <option value="Õpetaja">Õpetaja</option>
+                <option value="Treener">Treener</option>
+            </select>
+            <input type="text" class="form-input no-outline" placeholder="parool" name="parool" required>
+            <input type="text" class="form-input no-outline" placeholder="uuesti parool" name="uuesti_parool" required>
             <input type="submit" value="Loo konto" class="form-submit center no-outline">
         </form>
     </div>
--- Scripts/register.php
+++ Scripts/register.php
@@ -11,3 +11,43 @@ $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas)
 if($yhendus==false){
     die("Failed to connect with MySQL: ". mysqli_connect_errno());
 }
+
+//
+$email = $_POST['email'];
+$password = $_POST['parool'];
+$eesnimi = $_POST['eesnimi'];
+$perenimi = $_POST['perenimi'];
+$roll = $_POST['role'];
+$passwordRepeat = $_POST['uuesti_parool'];
+
+if($password!==$passwordRepeat){
+    echo("Passwords don't match");
+    header("Location: ../Pages/main.php");
+    exit();
+}
+
+// protect against sql injection
+$email = stripcslashes($email);
+$password = stripcslashes($password);
+
+$email = mysqli_escape_string($yhendus, $email);
+$password = mysqli_escape_string($yhendus, $password);
+
+// query that will be sent to mysql
+$query = "INSERT INTO `kasutaja` (`id`, `eesnimi`, `perenimi`, `Kontakt`, `Role`, `Parool`) VALUES (NULL, '$eesnimi', '$perenimi', '$email', '$roll', '$password');";
+
+// get query results from database
+$result = mysqli_query($yhendus, $query);
+
+// what happens after we get our query results
+if($yhendus->query($query) === TRUE){
+    ob_start();
+    header("Location: ../Pages/login.php");
+    ob_end_flush();
+    exit();
+}
+else {
+    echo "something went wrong";
+    header("Location: ../Pages/register.php");
+    exit();
+}
