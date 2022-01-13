83ba64255e1941e7e9003b700d17d140d3e788a2
Christo-IV <christo.vahtramae@khk.ee>
2022-01-13 11:41:08 +0200

    Slight inconsistency fix

--- Pages/register.php
+++ Pages/register.php
@@ -23,16 +23,16 @@ if ($_SESSION["status"]=='active') {
     <div class="container flex">
         <h1 class="smth center">Loo konto</h1>
         <form action="../Scripts/register.php" method="POST" class="flex">
-            <input type="text" class="form-input no-outline" placeholder="email" name="email" required>
-            <input type="text" class="form-input no-outline" placeholder="eesnimi" name="eesnimi" required>
-            <input type="text" class="form-input no-outline" placeholder="perenimi" name="perenimi" required>
+            <input type="text" class="form-input no-outline" placeholder="Email" name="email" required>
+            <input type="text" class="form-input no-outline" placeholder="Eesnimi" name="eesnimi" required>
+            <input type="text" class="form-input no-outline" placeholder="Perenimi" name="perenimi" required>
             <select class="form-input no-outline" name="role" required>
                 <option value="Õpilane">Õpilane</option>
                 <option value="Õpetaja">Õpetaja</option>
                 <option value="Treener">Treener</option>
             </select>
-            <input type="text" class="form-input no-outline" placeholder="parool" name="parool" required>
-            <input type="text" class="form-input no-outline" placeholder="uuesti parool" name="uuesti_parool" required>
+            <input type="text" class="form-input no-outline" placeholder="Parool" name="parool" required>
+            <input type="text" class="form-input no-outline" placeholder="Korrake parool" name="uuesti_parool" required>
             <input type="submit" value="Loo konto" class="form-submit center no-outline">
         </form>
     </div>
